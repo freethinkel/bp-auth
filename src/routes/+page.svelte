37a1933381f08e5a244d65f7c6cf907f9df9781d
Egor Kolesnikov <freethinkel@gmail.com>
2025-05-20 19:31:20 +0500

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,31 @@
-<script>
+<script lang="ts">
 	import { base } from '$app/paths';
 	import { MOCK_USERS } from '@/shared/helpers/mock-fetch';
 </script>
 
-<div class="container">
-	<h1>Welcome to AuthPage</h1>
-	<p>A secure authentication system built with best practices</p>
-	<p>You can use any of these demo accounts to test the login functionality:</p>
+<div class="page">
+	<div class="container">
+		<h1>Welcome to AuthPage</h1>
+		<p>A secure authentication system built with best practices</p>
+		<p>You can use any of these demo accounts to test the login functionality:</p>
 
-	<table>
-		<thead>
-			<tr
-				><th>Email</th>
-				<th>Password</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each MOCK_USERS as user (user.id)}
-				<tr>
-					<td>{user.email}</td>
-					<td>{user.password}</td>
+		<table>
+			<thead>
+				<tr
+					><th>Email</th>
+					<th>Password</th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
-	<br />
-	<a href="{base}/login">Go to Login page</a>
+			</thead>
+			<tbody>
+				{#each MOCK_USERS as user (user.id)}
+					<tr>
+						<td>{user.email}</td>
+						<td>{user.password}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<br />
+		<a href="{base}/login">Go to Login page</a>
+	</div>
 </div>
